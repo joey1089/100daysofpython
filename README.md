dd2390e57b0aaaa738050ca5f7b491d5c6a71ab1
Joseph Peter <josephvpeter7@gmail.com>
2024-02-01 13:20:04 -0500

    Improved Code

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 100daysofcode
 Python code building excerises with course from 100 days of code. I realized that I haven't added my past python code building excerises from the course.
-following lessons from https://www.udemy.com/course/100-days-of-code/
+# Course from 100 days of code
+Following lessons and concepts from https://www.udemy.com/course/100-days-of-code/
--- password_gen.py
+++ password_gen.py
@@ -4,26 +4,20 @@ import string
 import os
 
 letters = string.ascii_letters
-numbers = string.digits
+# numbers = string.digits
 symbols = string.punctuation
+TOTAL_CHARS = 17 # Constant
 
-# print("Letters : ",letters)
-# print("Numbers : ",numbers)
-# print("Symbols : ",symbols)
-# all_combine = letters+numbers+symbols
-# print("All combined together values : ",all_combine)
-
-
-# print("Create Strong Password using PyPassword Generator!")
-# print("Create a strong password that has more than 15 characters!!!")
-# nr_letters= int(input("How many letters would you like in your password?\n")) 
-# nr_symbols = int(input(f"How many symbols would you like?\n"))
-# nr_numbers = int(input(f"How many numbers would you like?\n"))
+print("Create Strong Password using PyPassword Generator!")
+print(f"Create a strong password that has more than {TOTAL_CHARS} characters!!!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #test values
-nr_letters = 10
-nr_numbers = 4
-nr_symbols = 3
+# nr_letters = 10
+# nr_numbers = 4
+# nr_symbols = 3
 total_char = nr_letters + nr_numbers + nr_symbols
 
 #Eazy Level - Order not randomised:
@@ -33,35 +27,29 @@ total_char = nr_letters + nr_numbers + nr_symbols
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
-while total_char > 16:
+while total_char > TOTAL_CHARS:
     print("Generating Password! ...")
     # Generate random letters
-
     pass_list = []
     for letter in range(0,nr_letters):
-        pass_list = random.sample(letters, k=nr_letters)       
-    # print("Random letters selected : ", pass_list, len(pass_list))
+        pass_list = random.sample(letters, k=nr_letters) # using random class method sample to get unique values instead of choice
+       
     # Generate random numbers
-    
-    pass_num = random.sample(range(0,10),k= nr_numbers)
-    # print("Random selected numbers : ", pass_num)
-    # print(pass_num)
+    pass_num = random.sample(range(0,10),k= nr_numbers) # using sample method with range to get unique numbers
     for no in pass_num:
-        pass_list.append(str(no))
-    # print("\n New List :- ",pass_list)
+        pass_list.append(str(no)) # resolved conflicts of int on the list, by converting int into str right here than to deal later on
+        # leave this code alone, its working and appends into the list as a string.
 
     # Generate random symbols
-
     for sym in range(0, nr_symbols):
         pass_list += random.choice(symbols)
-        # pass_list += random.sample(symbols,k=1)
-    # print("Random Selected symbols : ", pass_list, len(pass_list))
+        
 
     # check if total characters 
-    if total_char == len(pass_list):
-        print("\nTotal no of characters are the same!")
-    else:
-        print("Password generated has extra values")
+    # if total_char == len(pass_list):
+    #     print("\nTotal no of characters are the same!")
+    # else:
+    #     print("Password generated has extra values")
 
     # Generate password
     print("\n Before Random shuffle : ", pass_list, len(pass_list))
@@ -75,6 +63,6 @@ while total_char > 16:
     break
 
 else:
-    print("Choose more than 16 characters to create password")
+    print(f"Choose more than {TOTAL_CHARS} characters to create password")
 
     
