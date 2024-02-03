249bfcb16e91a8a9f692d2e9031ca3119dd09f97
Joseph Peter <josephvpeter7@gmail.com>
2024-02-03 17:38:36 -0500

    CodeImproved

--- password_gen_fn.py
+++ password_gen_fn.py
@@ -1,5 +1,6 @@
 # Using Functions to make it more efficient to Generate password.
 # Here trying to impliment another level of learning.
+import os
 import random
 import string
 # from password_gen import nr_letters,nr_numbers,nr_symbols, total_char
@@ -9,27 +10,57 @@ from wonderwords import RandomSentence
 # Declare all Constants 
 letters = string.ascii_letters
 symbols = string.punctuation
-TOTAL_CHARS = 20 # Constant caps
-userchoice = int(input(f"\n Want Strong Random password type '1' or want passphrases type '2' :-" ))
+no_letters = 12
+no_numbers = 5
+no_symbols = 3
+TOTAL_CHARS = 20 # Constant are all caps
+os.system('clear')
 
-print("Generate Strong Password with all Combinations!!!")
 
-
-#Generating Passpharse and randomize the caps on it
+#Generating passphrase and randomize the caps on it
 
 def passphrase():
-    '''Generating a passphrase.'''   
-    s = RandomSentence() 
-    #https://wonderwords.readthedocs.io/en/latest/api_docs/random_sentence.html
+    '''Generating a passphrase.'''      
+    # refer:- https://wonderwords.readthedocs.io/en/latest/api_docs/random_sentence.html
     # Get a random sentence with a subject, predicate, direct object and adjective
-    # s.sentence()
+    s = RandomSentence() 
     with_space_sentence = s.sentence()
-    print("\n Remember this Passpharse, also combine ur own sysmbols and numbers to it :- ", with_space_sentence)
+    print(f"\n Remember this Passpharse:-  {with_space_sentence} \n")
     rm_space_sentence = with_space_sentence.replace(".","").replace(" ","")
+    userchoice = input("Do you want just the Passphrase type '1' or Want to randomise with nos & symbols :- ")
+    if userchoice == 1:
+        print("\n You choose to go with the just the Passphrase! \n")
+        print("Terminating...")
+        exit()
+    else:
+        rm_space_sentence = with_space_sentence.replace(".","").replace(" ","") 
+        # here join is used to convert to a lower and upper string at random.
+        rm_space_sentence = ''.join(random.choice((str.upper, str.lower))(char) for char in rm_space_sentence)
+        print("Removed Space and Randomized Caps on the passphrase :- ",rm_space_sentence)
+        # add random numbers and sysmbols, next level 
+        pass_list = []
+        # Generate random numbers
+        pass_num = random.sample(range(0,10),k= no_numbers) 
+        for no in pass_num:
+            pass_list.append(str(no)) #
+
+        # Generate random symbols
+        for sym in range(0, no_symbols):
+            pass_list += random.choice(symbols)
+        # Adding the random numbers and symbols to the passphrase
+        print("Random Generate numbers and symbols :- ", pass_list)
+        convert2list = str(rm_space_sentence.split())
+        print(convert2list)
+        pass_list.append(convert2list)
+        print("After appending to the random list : ",pass_list)
+    
+        random.shuffle(pass_list)
+        add2passphrase = ""
+        add2passphrase = "".join(pass_list)
+        final_passphrase = add2passphrase.replace("[","").replace("]","").replace("'","")
+        print(f"\n Your randomized Passphrase:-  {final_passphrase} \n")
         
-    # here join is used to convert to a lower and upper string at random.
-    rm_space_sentence = ''.join(random.choice((str.upper, str.lower))(char) for char in rm_space_sentence)
-    print("Removed Space and Randomized Caps on the Passpharse :- ",rm_space_sentence)
+
 
 
 
@@ -40,42 +71,32 @@ def password_randomized():
     print("\n Generating Randomized Password.")
     # Generate random letters
     pass_list = []
-    for letter in range(0,nr_letters+1):
-        pass_list = random.sample(letters, k=8)       
-    # print("Random letters selected : ", pass_list, len(pass_list))
+    for letter in range(0,no_letters):
+        pass_list = random.sample(letters, k=no_letters) 
+       
     # Generate random numbers
-    
-    pass_num = random.sample(range(0,10),k= nr_numbers)
-    print("Random selected numbers : ", pass_num)
-    print(str(pass_num))
-    pass_list += str(pass_num)
+    pass_num = random.sample(range(0,100),k= no_numbers) 
+    for no in pass_num:
+        pass_list.append(str(no)) #
 
     # Generate random symbols
-    rand_symbols = []
-    for sym in range(0, nr_symbols+1):
-        rand_symbols += random.choice(symbols)
-        # pass_list += random.sample(symbols,k=1)
-    print(rand_symbols)
-    pass_list += rand_symbols
-    print("Random Selected symbols : ", pass_list, len(pass_list))
-    if total_char != len(pass_list):
-        print("Password generated has extra values")
-
+    for sym in range(0, no_symbols):
+        pass_list += random.choice(symbols)
+        
     # Generate password
-    print("\n Before Random shuffle : ", pass_list, len(pass_list))
-    pass_rand = random.shuffle(pass_list)
+    print(f"\n Before Random shuffle is {pass_list} and total characters is {len(pass_list)}") 
+    random.shuffle(pass_list)
     print("\n After Random : ", pass_list)
 
-    passwords = ""
-    for char in pass_list:
-        passwords += char
-    
-    print("final Generated password : ", passwords)
-    # print("Length of the password : ", len(passwords))
+    new_password = "".join(pass_list)
+    print("\n Your randomly generated password : ",new_password)
 
+
+print("Generate Strong Password or Passphrases!!!")
+userchoice = int(input(f"\n Want Strong Random password type '1' or want passphrases type '2' :- " ))
 if userchoice == 1:
     print("Proceeding to create strong randomized password")
-    # password_randomized()
+    password_randomized()
 else:
     print("Proceeding to create a passphrase")
     passphrase()
